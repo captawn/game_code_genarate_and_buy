b98cfd1700ef1716c781a89e58fc5803d1da65ba
nima <linux.vps.vpn@gmail.com>
2023-11-09 18:55:30 +0330

    def that generate_unique_code

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+import random
+import string
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def generate_unique_code(size=40):
+    characters = string.ascii_letters + string.digits
+    code = ''.join(random.choice(characters) for _ in range(size))
+    return code
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Example usage
+unique_code = generate_unique_code()
+print(unique_code)
